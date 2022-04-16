a4d4688405fa4678778d53e8ddef5d0444bec1b6
KonstaSlay <js.incubatorks@mail.com>
2022-04-17 00:13:06 +0200

    Code

---
+++ Thinkful - List Drills - Longest word.js
@@ -0,0 +1,17 @@
+/*Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+
+* For example:
+
+* ['simple', 'is', 'better', 'than', 'complex'] ==> 7
+
+Do not modify the input list.*/
+
+function longest(words) {
+    let num = 0;
+    for (let i =0; i < words.length; i++) {
+        if (words[i].length >= num) {
+            num = words[i].length;
+        }
+    }
+    return num
+}
